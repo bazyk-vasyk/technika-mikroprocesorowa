87ec7ec9fe356198268b1994393ad69756882a55
bazyk-vasyk <basildeynega@gmail.com>
2026-04-07 20:23:54 +0200

    5lab experiment bez przycisków

--- main.c
+++ main.c
@@ -1,22 +1,84 @@
 #include "STM8S208R_for_mac.h"
 
-main()
-{
-    //przycisk
-	PE_DDR &= ~(1<<4);
-	PE_CR1 |= (1<<4);
-	PE_CR2 |=(1<<4);
-	//led
-    PC_DDR |= 1 << 5;
-	PC_CR1 |= 1 << 5;
-	PC_CR2 &= ~( 1 << 5);
+#define D1 5 //PC5
+#define D2 7 //PC7
+#define D3 6 //PC6
+#define D4 5 //PE5
 
-    EXTI_CR2 |= (1 << 0);
+#define S1 4 //PB4
+#define S2 3 //PB3
+#define S3 2 //PB2
+
+unsigned short tick_msec;
+
+unsigned short tim4_divider = 0;
+unsigned int tim4_divider2 = 0;
+unsigned int tim4_divider3 = 0;
+
+void main()
+{   
+// D1
+	PC_DDR |= 1<<D1;
+	PC_CR1 |= 1<<D1;
+	PC_CR2 &= ~(1<<D1);
+	
+	// D2
+	PC_DDR |= 1<<D2;
+	PC_CR1 |= 1<<D2;
+	PC_CR2 &= ~(1<<D2);
+	
+	// D3
+	PC_DDR |= 1<<D3;
+	PC_CR1 |= 1<<D3;
+	PC_CR2 &= ~(1<<D3);
+	
+
+	
+	
+	// S1
+	PB_DDR &= ~(1<<S1);
+	PB_CR1 &= ~(1<<S1);
+	PB_CR2 &= ~(1<<S1);
+    // S2
+	PB_DDR &= ~(1<<S2);
+	PB_CR1 &= ~(1<<S2);
+	PB_CR2 &= ~(1<<S2);
+	
+    
+    // TIM4
+    TIM4_PSCR = 2;
+    TIM4_ARR = 249;
+    TIM4_IER |= 1 << 0;
+    TIM4_CR1 |= 1 << 0;
+    
     __asm__ ("rim");
-    while (1);
+while (1);
 }
 
-void foo(void) __interrupt(7){
-PC_ODR ^= (1 << 5);
+void foo(void) __interrupt(23){
+TIM4_SR &= ~(1 << 0);
+
+tim4_divider++;
+tim4_divider2++;
+tim4_divider3++;
+
+if (tim4_divider >= 5)
+{
+tim4_divider = 0;
+PC_ODR ^= (1 << D1);
+}
+
+if (tim4_divider2 >= 200)
+{
+tim4_divider2 = 0;
+PC_ODR ^= (1 << D2);
+}
+
+if (tim4_divider3 >= 6000)
+{
+tim4_divider3 = 0;
+PC_ODR ^= (1 << D3);
+}
+
 return;
 }

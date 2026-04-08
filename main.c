e42eef1b4461b16cdee14979bbc1456b58012323
bazyk-vasyk <basildeynega@gmail.com>
2026-04-08 17:52:39 +0200

    50% lab 5 full full

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ unsigned short tim4_divider = 0;
 unsigned int tim4_divider2 = 0;
 unsigned int tim4_divider3 = 0;
 unsigned int tim4_divider4 = 0;
-unsigned char led_status = 0;
+unsigned char led_status = 1;
 unsigned char zliczanie=0;
 volatile unsigned char zliczanie3=0;
 
@@ -57,72 +57,63 @@ void main()
 	
     
     // TIM4
-    TIM4_PSCR = 1;
-    TIM4_ARR = 149;
+    TIM4_PSCR = 4;
+    TIM4_ARR = 187;
     TIM4_IER |= 1 << 0;
     TIM4_CR1 |= 1 << 0;
     
     __asm__ ("rim");
 while (1)
 {
-if ((PB_IDR & (1<<S1)) == 0) 
+   if ((PB_IDR & (1<<S1)) == 0) 
     {
             for(volatile long d=0; d<10000; d++); // delay
-            if ((PB_IDR & (1<<S1)) == 0) {
-                zliczanie++;
-                if(zliczanie>=2)
+            if ((PB_IDR & (1 << S1)) == 0) 
+                if(led_status == 1)
                 {
-                    zliczanie=0;
+                    led_status = 0; 
                 }
-                if(zliczanie==1)
+                else 
                 {
-                    led_status=1;
+                    led_status = 1; 
                 }
-                else
+                
+                // Czekaj na puszczenie przycisku S1
+                while((PB_IDR & (1 << S1)) == 0)
                 {
-                led_status=0;
-                 PC_ODR |= (1<<D1) | (1<<D2) | (1<<D3);
-                 PE_ODR |=(1<<D4);
+                    delay(10);
                 }
-            }
-            while((PB_IDR & (1<<S1)) == 0)
-            {
-                delay(10);
-            }
     }
-/*if ((PB_IDR & (1<<S2)) == 0) 
-    {
-            for(volatile long d=0; d<10000; d++); // delay
-            if ((PB_IDR & (1<<S2)) == 0) {
-                led_status=0;
-                 PC_ODR |= (1<<D1) | (1<<D2) | (1<<D3);
-                 PE_ODR |=(1<<D4);
-            }
-    }*/
+    
    if ((PB_IDR & (1<<S3)) == 0) 
     {
-            for(volatile long d=0; d<10000; d++); // delay
+        for(volatile long d = 0; d < 10000; d++); // delay
+        if ((PB_IDR & (1 << S3)) == 0) 
+        {
             zliczanie3++;
-            if(zliczanie3==1)
+            if(zliczanie3 == 1)
             {
-                 delay(10);
+                TIM4_CR1 &= ~(1 << 0); 
             }
-            if(zliczanie3>=2)
-                {
-                    zliczanie3=0;
-                }
+            else 
+            {
+                zliczanie3 = 0;
+                TIM4_CR1 |= (1 << 0);
+            }
             while((PB_IDR & (1<<S3)) == 0)
             {
                 delay(10);
             }
             
+        }
     }
 }
+
 }
 void foo(void) __interrupt(23)
 {
     TIM4_SR &= ~(1 << 0);
-    if(led_status==0|| zliczanie3 == 1) 
+    if(led_status==0) 
         {   
             return;
         }
@@ -148,7 +139,7 @@ if (tim4_divider3 >= 444)
 tim4_divider3 = 0;
 PC_ODR ^= (1 << D3);
 }
-if (tim4_divider4 >= 20000)
+if (tim4_divider4 >= 2000)
 {
 tim4_divider4 = 0;
 PE_ODR ^= (1 << D4);
